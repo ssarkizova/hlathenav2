0ac6dffff265f573e3eedc656c77fa08bf9babda
Forman <clf176@compute-f-17-10.o2.rc.hms.harvard.edu>
2023-05-12 14:53:33 -0400

    Moving gene annotation to annotate module

--- hlathena/__init__.py
+++ hlathena/__init__.py
@@ -6,5 +6,5 @@ __credits__ = 'The Broad Institue and Dana-Farber Cancer Institute'
 from hlathena.plotting import plot_length, plot_logo, plot_umap
 from hlathena.peptide_projection import PCA_encode
 from hlathena.predict import predict
-from hlathena.annotate import list_expression_references, add_tcga_expression
+from hlathena.annotate import list_expression_references, get_reference_gene_ids, add_tcga_expression
 from hlathena.peptide_dataset import PeptideDataset
--- hlathena/annotate.py
+++ hlathena/annotate.py
@@ -16,7 +16,74 @@ def list_expression_references() -> pd.DataFrame:
     """
     ref_expr_info = str(exp_references.joinpath(f'tcga_abbreviations.txt'))
     return pd.read_table(ref_expr_info, sep='\t', index_col='Study Abbreviation')
-    
+
+
+def get_reference_gene_ids(pep_df: pd.DataFrame, \
+                            pep_col: str = 'seq', \
+                            ref_fasta: str = None, \
+                            add_context: bool = True):
+        """
+        Given a reference FASTA file and a list of peptide sequences, this function identifies the gene(s) 
+        in the reference file that produce each peptide sequence, and returns a DataFrame with information 
+        about the peptide, the corresponding gene(s), and (optionally) flanking amino acid sequences.
+
+        Args:
+            pep_df (pd.DataFrame): Dataframe with peptide sequences
+            pep_col (str): Name of peptide column
+            ref_fasta (str): Path to a reference fasta containing protein sequences. If None, 
+                a default fasta file is used.
+            add_context (bool): If True, add columns to the output DataFrame containing the 30 amino acids 
+                upstream and downstream of each peptide sequence.
+
+        Returns:
+            A DataFrame with columns for the peptide sequence, corresponding gene(s), 
+                and optional flanking amino acid sequences.
+        """
+        seqs = list(pep_df[pep_col])
+
+        automaton = ahocorasick.Automaton()
+        for idx, key in enumerate(seqs):
+            automaton.add_word(key, (idx, key))
+
+        automaton.make_automaton()
+        
+        if ref_fasta is None:
+            references = importlib_resources.files('hlathena').joinpath('references')
+            ref_fasta = str(references.joinpath(f'HUGO_proteome.fa'))
+        record_dict = SeqIO.index(ref_fasta, "fasta")
+        
+        new_df = []
+        for idx, key in enumerate(record_dict):
+            hugo = record_dict[key].name.split("|")[1]
+            seq = str(record_dict[key].seq)
+            for end_index, (insert_order, original_value) in automaton.iter(seq):
+                start_index = end_index - len(original_value) + 1
+                peptide_info = [original_value, hugo]
+
+                if add_context:
+                    ctx_up_start = start_index - 30 if start_index > 30 else 0
+                    ctx_down_end = end_index + 30 if len(seq) > end_index + 30 else len(seq)
+
+                    up_seq = seq[ctx_up_start:start_index]
+                    dn_seq = seq[end_index+1:ctx_down_end]
+
+                    if len(up_seq) < 30:
+                        up_seq = "-"*(30-len(up_seq)) + up_seq
+                    if len(dn_seq) < 30:
+                        dn_seq = dn_seq + "-"*(30-len(dn_seq))
+
+                    peptide_info += [up_seq, dn_seq]
+                    if not peptide_info in new_df:
+                        new_df.append(peptide_info)
+                else:
+                    if not peptide_info in new_df:
+                        new_df.append(peptide_info)
+        if add_context:
+            return pd.DataFrame(new_df, columns=['seq','Hugo_Symbol','ctex_up','ctex_dn'])
+        else:
+            return pd.DataFrame(new_df, columns=['seq','Hugo_Symbol'])
+        
+
     
 def add_tcga_expression(pep_df: pd.DataFrame, cancer_type: str, hugo_col: str = 'Hugo_Symbol') -> pd.DataFrame:
     """Add column with TCGA reference expression data.
